320d8716c45c0dd3bdf40ade865a16f7eec236f8
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-08-21 23:49:42 -0500

    install vue-router

--- frontend/src/services/players.ts
+++ frontend/src/services/players.ts
@@ -2,7 +2,8 @@ import axios from '../util/apiClient'
 const baseUrl = '/players'
 
 const getAllInjuries = async () => {
-  const response = await axios.post(baseUrl)
+  const response = await axios.get(baseUrl)
+  console.log(response)
   return response.data
 }
 
