8d51158f6877c94f7f9ee1b13c3830e953a63207
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-08-28 12:06:22 -0500

    add getAllPlayersFromRoster

--- frontend/src/services/sleeperUser.ts
+++ frontend/src/services/sleeperUser.ts
@@ -15,9 +15,26 @@ const getSleeperUserLeagues = async (userId: string) => {
 const getSleeperUserRosterFromLeague = async (userId: string, leagueId: string) => {
     const response = await fetch (`https://api.sleeper.app/v1/league/${leagueId}/rosters`)
     const data = await response.json()
-    const userRoster = data.filter((t: any) => t.owner_id == userId)
-    const { players, reserve} = userRoster
-    return { players, reserve }
+    const userRoster = data.find((t: any) => t.owner_id == userId)
+    const { roster, reserve} = userRoster
+    return { roster, reserve }
 }
 
-export default { getSleeperUser, getSleeperUserLeagues}
+const getAllPlayersFromRoster = async (roster: string[], reserve: string[]) => {
+    const response = await fetch (`https://api.sleeper.app/v1/players/nfl`)
+    const data = await response.json()
+    let players: {[key: string]: any} = {}
+    for(let i=0; i<roster.length; i++){
+        const playerToAdd = data.roster[i]
+        players[roster[i]] = playerToAdd
+    }
+    for (let i = 0; i < reserve.length; i++) {
+        const playerToAdd = data[reserve[i]]
+        if (playerToAdd) {
+            players[reserve[i]] = playerToAdd
+        }
+    }
+    return players
+}
+
+export default { getSleeperUser, getSleeperUserLeagues, getSleeperUserRosterFromLeague, getAllPlayersFromRoster }
