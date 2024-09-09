ca2a449393fda8cd6eb332bc6e35bc869b30cb6e
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-09-09 12:30:17 -0500

    change api endpoint for getWeather function

--- frontend/src/services/weather.ts
+++ frontend/src/services/weather.ts
@@ -5,7 +5,7 @@ const getWeather = async (stadium: Stadium) => {
         if (stadium.dome){
             return 'dome'
         }
-    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${stadium.lat}&lon=${stadium.lon}&appid=${import.meta.env.VITE_OPENWEATHERMAP_API_KEY}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${stadium.lat}&lon=${stadium.lon}&appid=${import.meta.env.VITE_OPENWEATHERMAP_API_KEY}`)
     const data = await response.json()
 
     //main.temp, weather[0].description, weather[0].icon, wind.speed, wind.gust, and clouds.all
