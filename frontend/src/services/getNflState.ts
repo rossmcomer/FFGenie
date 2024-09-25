23fb7ad8d89ea671d6b98b7a5651e3a1dad4139c
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2024-09-24 20:26:14 -0500

    add getNflState function

---
+++ frontend/src/services/getNflState.ts
@@ -0,0 +1,8 @@
+const getNflState = async () => {
+    const response = await fetch (`https://api.sleeper.app/v1/state/nfl`)
+    const data = await response.json()
+
+    return data
+}
+
+export default { getNflState }
