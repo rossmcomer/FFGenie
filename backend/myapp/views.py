7765d23b64f208a6aa587bd8753c1b79c1148a24
Ross Comer <122069684+rossmcomer@users.noreply.github.com>
2024-09-25 16:04:49 -0500

    Delete backend directory

--- backend/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from .controllers.injuries import get_all_injuries_data
-
-def injuries(request):
-    all_injuries_data = get_all_injuries_data()
-
-    print(all_injuries_data[0])
-    return JsonResponse({'all_injuries_data': all_injuries_data})
